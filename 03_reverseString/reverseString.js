818584eefc44e4ed0ab9271895aad234b570f757
joshualipton <jalipton11@gmail.com>
2022-08-03 21:21:58 -0700

    Complete reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(s) {
+    res = ''
+    sArr = s.split('');
 
+    for (let i = sArr.length - 1; i > -1; --i) {
+        res += sArr[i];
+    }
+
+    return res
 };
 
 // Do not edit below this line
