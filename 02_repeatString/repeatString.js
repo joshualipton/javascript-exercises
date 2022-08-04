ba263a9819e42055fcf2f289a4f77853cf0665e9
joshualipton <jalipton11@gmail.com>
2022-08-03 21:14:43 -0700

    Complete repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, n) {
+    res = '';
 
+    if (n < 0) {
+        return 'ERROR';
+    }
+    for (let i = 0; i < n; ++i) {
+        res += string;
+    }
+    return res;
 };
 
 // Do not edit below this line
