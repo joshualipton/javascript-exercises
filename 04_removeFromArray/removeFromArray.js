4848c08a02186789b6203a2235e929819eb249e0
joshualipton <jalipton11@gmail.com>
2022-08-03 21:33:57 -0700

    Complete removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    const newArr = []
+    for (let i = 0; i < arr.length; ++i) {
+        if (!args.includes(arr[i])) {
+            newArr.push(arr[i]);
+        }
+    }
 
+    return newArr;
 };
 
 // Do not edit below this line
